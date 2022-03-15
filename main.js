a07b9a38cc33f49cc1db9072de6d3a24ae309094
Chris Stavitsky <corridorr@gmail.com>
2022-03-15 13:32:53 -0700

    Cleanup, throw error if auth token not set

--- main.js
+++ main.js
@@ -9,10 +9,17 @@ require('./sentry');
 let date = Date.now();
 
 // replace with your sentry_key ref: https://develop.sentry.dev/sdk/store/
-const SENTRY_KEY='https://aab0a4e2af5a4df0a9adf094e657809e@o87286.ingest.sentry.io/1318230'
-const SENTRY_AUTH_TOKEN="YOUR_AUTH_TOKEN"
+const SENTRY_KEY='aab0a4e2af5a4df0a9adf094e657809e@o87286' // for testorg-az 'electron' project
+const PROJECT_ID='1318230' // for testorg-az 'electron' project
+const SENTRY_AUTH_TOKEN="" //Put your auth token here
 const dir = app.getAppPath();
 const pathToDir = path.join(__dirname, "offlineEvents")
+
+if (!SENTRY_KEY) {
+  throw("You must set your SENTRY_KEY in main.js")
+} else if (!SENTRY_AUTH_TOKEN) {
+  throw("You must set your SENTRY_AUTH_TOKEN in main.js")
+}
 
 const removeDir = function(path) {
   if (fs.existsSync(path)) {
@@ -105,7 +112,7 @@ ipcMain.on('online-status-changed', (event, status) => {
           fs.readFile("./offlineEvents/" + file, "utf8", function (err, data) {
             if (err) throw err;
             // replace with your project store endpoint https://develop.sentry.dev/sdk/store/ and Auth Token
-            axios.post(`https://${SENTRY_KEY}.ingest.sentry.io/api/PROJECT_ID/store/`, data, {
+            axios.post(`https://${SENTRY_KEY}.ingest.sentry.io/api/${PROJECT_ID}/store/`, data, {
               headers: {
                 'Content-Type': 'application/json',
                 'X-Sentry-Auth': `Sentry sentry_version=7,sentry_timestamp=${date},sentry_client=sentry-curl/1.0,sentry_key=${SENTRY_KEY}`,
